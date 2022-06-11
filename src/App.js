efd0c8423bcaf8fbacc44fc3a3d67f3ced683861
Iyana Marquez <yayakix@gmail.com>
2022-06-11 18:12:57 -0400

    navbar links

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import './App.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Stocks from './components/Stocks';
 
 function App() {
   return (
     <div className="App">
-    <Route path='/'>
-    <Home/>
-    </Route>
-   
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/stonks">
+          <Stocks />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
 function Navbar() {
-  return <></>;
+  return (
+    <>
+      <div className="nav">
+        <Link to="/">
+          <div className="title">IStonks</div>
+        </Link>
+        <Link to="/about">
+          <div className="navitem">About</div>
+        </Link>
+        <Link to="/stonks">
+          <div className="navitem">Stonks</div>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
